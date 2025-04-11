8b58eb70a8e84b2e0afe466d875f31b3d879ae22
samorobo <samgodwin111@gmail.com>
2025-04-11 07:14:26 +0100

    fix and added a button that navigates me to the swiping card section from

--- src/components/TapToReveal.vue
+++ src/components/TapToReveal.vue
@@ -35,7 +35,7 @@
       <!-- Next Button -->
       <button 
         class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded"
-        @clickk="goToSwipeQuiz"
+        @click="goToSwipeQuiz"
       >
         Next →
       </button>
